b866ebeac00e9f1772050d15fa65a2a986569d0d
Marek Jenda <edyp87@gmail.com>
2018-07-07 00:41:42 +0200

    Fixed missing form parsing.

--- modules/handlers.js
+++ modules/handlers.js
@@ -3,10 +3,14 @@ var formidable = require('formidable');
 
 exports.upload = function(request, response) {
     console.log("Rozpoczynam obsługę żądania upload.");
-    fs.readFile('templates/upload.html', function(err, html) {
-        response.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
-        response.write(html);
-        response.end();
+    var form = new formidable.IncomingForm();
+    form.parse(request, function(error, fields, files) {
+        fs.renameSync(files.upload.path, "test.png");
+        fs.readFile('templates/upload.html', function(err, html) {
+            response.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
+            response.write(html);
+            response.end();
+        });
     });
 }
 
